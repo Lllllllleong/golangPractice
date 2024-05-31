6b05920ba4b36135e72191edb7de9ab40a121103
Leong Yin Chan <u6858120@anu.edu.au>
2024-05-31 12:38:33 +1000

    LC Easy - Leaf-Similar Trees

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -105,6 +105,30 @@ public class Practice4 {
     }
 
 
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        collectLeaves(root1, leaves1);
+        collectLeaves(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    private void collectLeaves(TreeNode node, List<Integer> leaves) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+        } else {
+            collectLeaves(node.left, leaves);
+            collectLeaves(node.right, leaves);
+        }
+    }
+
+
+
+
+
 
 
     public static void main(String[] args) {
