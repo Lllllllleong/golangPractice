0f394aba8a3e8e048ffbb4db996c1a917cabedb1
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-19 14:24:36 +0800

    LC Easy - N-th Tribonacci Number

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -2016,6 +2016,23 @@ public class Practice4 {
 
 
 
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int index = 3; index <= n; index++) {
+            dp[index] = dp[index - 1] + dp[index - 2] + dp[index - 3];
+        }
+
+        return dp[n];
+    }
+
+
 
 
 
