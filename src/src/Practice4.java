912798ee0010d52361a4533c9b4baf512cde964e
Leong Yin Chan <u6858120@anu.edu.au>
2024-06-19 15:25:27 +0800

    LC Medium - Minimum Area Rectangle Slow

--- src/src/Practice4.java
+++ src/src/Practice4.java
@@ -2051,6 +2051,28 @@ public class Practice4 {
         return dp[0][0];
     }
 
+    public int minAreaRect(int[][] points) {
+        Set<String> set = new HashSet<>();
+        for (int[] point : points) {
+            set.add(point[0] + "," + point[1]);
+        }
+        int minArea = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+
+                // Check if the points can form a diagonal of a rectangle
+                if (x1 != x2 && y1 != y2) {
+                    if (set.contains(x1 + "," + y2) && set.contains(x2 + "," + y1)) {
+                        int area = Math.abs((x2 - x1) * (y2 - y1));
+                        minArea = Math.min(minArea, area);
+                    }
+                }
+            }
+        }
+        return (minArea == Integer.MAX_VALUE) ? 0 : minArea;
+    }
 
 
 
