0890773d0b6ba850e11bc686f35103ee53d9904e
Leong Yin Chan <u6858120@anu.edu.au>
2024-07-11 22:28:02 +1000

    LC Medium - Reverse Words in a String II

--- src/src/Practice5.java
+++ src/src/Practice5.java
@@ -1359,6 +1359,27 @@ public class Practice5 {
         return result;
     }
 
+
+    public void reverseWords(char[] s) {
+        reverse(s, 0, s.length - 1);
+        int start = 0;
+        for (int end = 0; end <= s.length; end++) {
+            if (end == s.length || s[end] == ' ') {
+                reverse(s, start, end - 1);
+                start = end + 1;
+            }
+        }
+    }
+
+    private void reverse(char[] s, int left, int right) {
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
     /**
      * Main Method
      */
