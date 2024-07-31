85bf2a76c4e7ebdd788dc6492c89bbf5e7ca43df
Leong Yin Chan <u6858120@anu.edu.au>
2024-07-31 18:48:31 +1000

    Medium - Maximum Array Hopping Score I
    DP

--- src/src/Practice5.java
+++ src/src/Practice5.java
@@ -2817,6 +2817,17 @@ public class Practice5 {
         return output;
     }
 
+    public int maxScore(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i+1; j < n; j++) {
+                dp[i] = Math.max(dp[i], ((j-i) * nums[j]) + dp[j]);
+            }
+        }
+        return dp[0];
+    }
+
 
 
 
