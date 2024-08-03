e3d30ba7db89760fa96760c644c792160d6cba5b
Leong Yin Chan <u6858120@anu.edu.au>
2024-08-04 02:32:03 +1000

    SQL Easy - Average Time of Process per Machine

--- src/src/SQL/SQLPractice.sql
+++ src/src/SQL/SQLPractice.sql
@@ -55,3 +55,28 @@ JOIN
     Weather w2
 ON
     DATEDIFF(w1.recordDate, w2.recordDate) = 1 && w1.temperature > w2.temperature;
+
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) as processing_time
+FROM
+    Activity a,
+    Activity b
+WHERE
+    a.machine_id = b.machine_id
+    AND a.process_id = b.process_id
+    AND a.activity_type = start
+    AND b.activity_type = end;
+
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) as processing_time
+FROM
+    Activity a,
+    Activity b
+WHERE
+    a.machine_id = b.machine_id
+    AND a.process_id = b.process_id
+    AND a.activity_type = 'start'
+    AND b.activity_type = 'end'
+GROUP BY machine_id;
