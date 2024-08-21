d59e66428b75e9749dc50f5feaae82483ea5339c
Leong Yin Chan <u6858120@anu.edu.au>
2024-08-21 15:20:16 +1000

    SQL Easy - Primary Department for Each Employee

--- src/src/SQL/SQLPractice.sql
+++ src/src/SQL/SQLPractice.sql
@@ -1,6 +1,23 @@
 --
 -- SQL Practice
 --
+(SELECT
+    employee_id,
+    department_id
+FROM
+    Employee
+WHERE
+    primary_flag = 'Y')
+UNION
+(SELECT
+    employee_id,
+    department_id
+FROM
+    Employee
+GROUP BY
+    employee_id
+HAVING
+    COUNT(employee_id) = 1);
 
 SELECT
     b.employee_id,
