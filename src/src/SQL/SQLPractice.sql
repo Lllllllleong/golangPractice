081f648ccf4003422e6d3cfa181ac1cd01da4daa
Leong Yin Chan <u6858120@anu.edu.au>
2024-08-21 15:38:33 +1000

    SQL Easy - Employees Whose Manager Left the Company

--- src/src/SQL/SQLPractice.sql
+++ src/src/SQL/SQLPractice.sql
@@ -2,6 +2,17 @@
 -- SQL Practice
 --
 SELECT
+    a.employee_id
+FROM
+    Employees a LEFT JOIN Employees b ON a.manager_id = b.employee_id
+WHERE
+    a.salary < 30000 AND a.manager_id IS NOT NULL AND b.employee_id IS NULL
+ORDER BY
+   a.employee_id;
+
+
+
+SELECT
     x,
     y,
     z,
