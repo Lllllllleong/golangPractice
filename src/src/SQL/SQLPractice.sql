27b10763c0030289749e479c2ca95e81aa9bf4bc
Leong Yin Chan <u6858120@anu.edu.au>
2024-10-27 15:17:32 +1100

    COMP2400 Final Exam Study - SQL Easy - The Latest Login in 2020

--- src/src/SQL/SQLPractice.sql
+++ src/src/SQL/SQLPractice.sql
@@ -2,6 +2,17 @@
 -- SQL Practice
 --
 
+SELECT
+    user_id,
+    MAX(time_stamp) AS last_stamp
+FROM
+    Logins
+WHERE
+    YEAR(time_stamp) = 2020
+GROUP BY
+    user_id;
+
+
 
 SELECT
     customer_id
