30284de6cb6b4cf9ee905371acc2212d221330aa
Leong Yin Chan <u6858120@anu.edu.au>
2025-06-19 11:23:37 +1000

    Go - Easy: Two Sum

--- src/main.go
+++ src/main.go
@@ -6,3 +6,16 @@ func main() {
     fmt.Println("!!")
 
 }
+
+
+func twoSum(nums []int, target int) []int {
+    numberMap := make(map[int]int)
+    for currentIndex, currentNumber := range nums {
+        requiredNumber := target - currentNumber
+        if secondIndex, exists := numberMap[requiredNumber]; exists {
+            return []int{secondIndex, currentIndex}
+        }
+        numberMap[currentNumber] = currentIndex
+    }
+    return nil
+}
