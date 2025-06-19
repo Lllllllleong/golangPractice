0e8eded05b3950c537ac487d646ef22d9d88928c
Leong Yin Chan <u6858120@anu.edu.au>
2025-06-19 10:50:30 +1000

    Second commit

--- src/main.go
+++ src/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to the Learning Go application!")
-    // Initialize application configurations or routes here
+    fmt.Println("!")
+
 }
