630a7175d5c494e5fa9869077ccc974e5ce1ad37
Lllllllleong <chanleongyin8@gmail.com>
2025-11-12 14:01:11 +1100

    Easy: 3541. Find Most Frequent Vowel and Consonant

--- easy/solutions.go
+++ easy/solutions.go
@@ -12,6 +12,35 @@ Space Complexity: O()
 
 /*
 ============================================================
+3541. Find Most Frequent Vowel and Consonant
+============================================================
+Time Complexity: O(n)
+Space Complexity: O(k) where k is the number of unique characters
+*/
+func maxFreqSum(s string) int {
+	vowelSet := map[rune]bool{
+		'a': true,
+		'e': true,
+		'i': true,
+		'o': true,
+		'u': true,
+	}
+	var vowelCount, consonantCount int
+	runeMap := make(map[rune]int)
+	for _, rune := range s {
+		runeMap[rune]++
+		if vowelSet[rune] && runeMap[rune] > vowelCount {
+			vowelCount = runeMap[rune]
+		}
+		if !vowelSet[rune] && runeMap[rune] > consonantCount {
+			consonantCount = runeMap[rune]
+		}
+	}
+	return vowelCount + consonantCount
+}
+
+/*
+============================================================
 3731. Find Missing Elements
 ============================================================
 Time Complexity: O(n log n)
