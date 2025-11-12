0a484c7cd4cc5eee38133c24a3244fb1a2af0cd9
Lllllllleong <chanleongyin8@gmail.com>
2025-11-12 12:23:33 +1100

    Restructure: Switch to mono-file approach for solutions

---
+++ hard/solutions.go
@@ -0,0 +1,17 @@
+package hard
+
+/*
+============================================================
+Add your hard difficulty solutions here
+============================================================
+
+Example format:
+
+Problem XXX: Problem Name
+Link: https://leetcode.com/problems/problem-name/
+
+[Problem description]
+
+Time Complexity: O(?)
+Space Complexity: O(?)
+*/
