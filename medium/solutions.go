49b01ea2486cad8eaa409f4a4ffe3ad53237dae0
Lllllllleong <chanleonggyin8@gmail.com>
2025-11-12 13:30:59 +1100

    feat: Add solution for Minimum Operations to Transform String

--- medium/solutions.go
+++ medium/solutions.go
@@ -2,16 +2,26 @@ package medium
 
 /*
 ============================================================
-Add your medium difficulty solutions here
+
 ============================================================
-
-Example format:
-
-Problem XXX: Problem Name
-Link: https://leetcode.com/problems/problem-name/
-
-[Problem description]
-
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O()
+Space Complexity: O()
 */
+
+/*
+============================================================
+3675. Minimum Operations to Transform String
+============================================================
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+func minOperations(s string) int {
+	numOperations := 0
+	for _, rune := range s {
+		currentCost := int(26-(rune-'a')) % 26
+		if currentCost > numOperations {
+			numOperations = currentCost
+		}
+	}
+	return numOperations
+}
