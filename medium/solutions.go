bee233ba3e5c5ebdcbc1dcd7428264b35f9fdc82
Lllllllleong <chanleonggyin8@gmail.com>
2025-11-13 14:12:56 +1100

    Medium: 3597. Partition String

--- medium/solutions.go
+++ medium/solutions.go
@@ -10,6 +10,35 @@ Space Complexity: O()
 
 /*
 ============================================================
+3597. Partition String
+============================================================
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+*/
+func partitionString(s string) []string {
+	runes := []rune(s)
+	n, i := len(runes), 0
+	seenSet := make(map[string]struct{})
+	output := []string{}
+	for i < n {
+		j := i + 1
+		for j <= n {
+			currentSubstring := string(runes[i:j])
+			if _, ok := seenSet[currentSubstring]; ok {
+				j++
+			} else {
+				seenSet[currentSubstring] = struct{}{}
+				output = append(output, currentSubstring)
+				break
+			}
+		}
+		i = j
+	}
+	return output
+}
+
+/*
+============================================================
 3675. Minimum Operations to Transform String
 ============================================================
 Time Complexity: O(n)
